755518cabdd85ee5996582d4611bfc715d19e97f
YasZamora <yzamo001@gmail.com>
2022-02-21 01:00:00 +0000

    mi tarea

--- src/models.py
+++ src/models.py
@@ -1,6 +1,6 @@
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Table, Column, ForeignKey, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
@@ -8,23 +8,46 @@ from eralchemy import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
+tabla_pivote1 = Table('personaje_favorito', Base.metadata,
+    Column('usuario_id', ForeignKey('usuario.id'), primary_key=True),
+    Column('personaje_id', ForeignKey('personaje.id'), primary_key=True))
+
+tabla_pivote2 = Table('planeta_favorito', Base.metadata,
+    Column('usuario_id', ForeignKey('usuario.id')),
+    Column('planeta_id' , ForeignKey('planeta.id')))
+
+class Usuario(Base):
+    __tablename__ = 'usuario'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
+    contrasena = Column(String(100), nullable=False)
+    # personaje_id = Column(Integer, ForeignKey('personaje.id'))
+    personaje = relationship('Personaje', secondary=tabla_pivote1)
+    # planeta_id = Column(Integer, ForeignKey('planeta.id'))
+    planeta = relationship('Planeta' , secondary=tabla_pivote2)
 
-class Address(Base):
-    __tablename__ = 'address'
+class Personaje(Base):
+    __tablename__ = 'personaje'
     # Here we define columns for the table address.
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    name = Column(String(250), nullable=False)
+    birth_year = Column(String(100), nullable=True)
+    eye_color = Column(String(100), nullable=True)
+   
+
+class Planeta(Base):
+    __tablename__ = 'planeta'
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    name_planeta = Column(String(250), nullable=False)
+    gravity = Column(String(100), nullable=True)
+    climate = Column(String(100), nullable=True)
+   
+  
 
     def to_dict(self):
         return {}
